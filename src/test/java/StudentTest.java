71e6479f1e7a3aea6854473ed75a49f09cd92d1f
Cristian <cristigroza98@gmail.com>
2020-03-15 23:32:02 +0200

    change pom

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -3,6 +3,8 @@ import Repository.StudentRepo;
 
 import Service.ServiceStudent;
 import Validator.StudentValidator;
+import Validator.ValidationException;
+import Validator.Validator;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,21 +17,25 @@ public class StudentTest {
     @Before
     public void setUp(){
         s1 = new Student("22","Ana",933,"ana@ana.com","Ioan");
-        s2 = new Student("22", "Mihai", 932, "iona@ioana.com","Mirabela");
+        s2 = new Student("25", "Mihai007", 932, "mihai@mahai.com","Mirabela");
         repo = new StudentRepo(new StudentValidator(),"C:\\Facultate\\An III\\SSVV\\src\\main\\resources\\studenti.xml");
         service = new ServiceStudent(repo);
     }
 
     @Test
     public void addStudent1(){
-        service.add(s1);
-        Assert.assertEquals("Ana", service.find("22").getNume());
+        Student s = service.add(s1);
+        Assert.assertEquals(s.getID(), "22");
     }
 
     @Test
     public  void addStudent2() {
-        int length = repo.size();
-        service.add(s2);
-        Assert.assertEquals(length, repo.size());
+        try {
+            service.add(s2);
+        } catch (ValidationException e)
+        {
+            Assert.assertEquals("\nNume invalid", e.getMessage());
+        }
+
     }
 }
